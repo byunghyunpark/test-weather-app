753f18b8e59ea4c7ba2e5be6a7f31b54174ca2ae
byunghyunpark <openmind8735@gmail.com>
2017-08-09 10:38:15 +0900

    Add search view

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,16 @@
-angular.module('App', ['ionic'])
+var app = angular.module('App', ['ionic'])
+
+app
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('search', {
+      url: '/search',
+      controller: 'SearchController',
+      templateUrl: 'views/search/search.html'
+    });
+
+    $urlRouterProvider.otherwise('/search');
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
